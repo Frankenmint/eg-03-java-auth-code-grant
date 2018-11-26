e2e137e2a8bad1bea07a94841268ad6bff6e3ad1
Shadi Massalha <shady.massalha@gmail.com>
2018-11-26 11:32:25 +0200

    rename example 14 files name

--- src/main/java/com/docusign/controller/examples/EG014Controller﻿CollectPayment.java
+++ src/main/java/com/docusign/controller/examples/EG014Controller﻿CollectPayment.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 
 @Controller
 @RequestMapping("/eg014")
-public class EG014Controller extends EGController {
+public class EG014Controller﻿CollectPayment extends EGController {
     @Override
     protected void addSpecialAttributes(ModelMap model) {
         model.addAttribute("gatewayOk", null != config.gatewayAccountId);
